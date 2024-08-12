70c4450489989b3b2c03e3fa40637d3393ba6dd6
unknown <vleonediaz@gmail.com>
2024-08-12 15:38:12 -0300

    agregando alerta al div

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const miDiv = document.getElementById("miDiv");
+
+miDiv.addEventListener('click', function() {
+    alert('Hola! Soy el div');
+});
+});
